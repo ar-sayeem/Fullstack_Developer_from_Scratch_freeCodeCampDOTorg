dc39ed14adfcf9310116d21dd139dc3ecb1799d2
ar-sayeem <adnan.rahman.sayeem@gmail.com>
2026-03-12 23:32:53 +0600

    9_Build_A_Blackjack_Game_Till_09:51:05

--- 9_Build_A_Blackjack_Game/index.js
+++ 9_Build_A_Blackjack_Game/index.js
@@ -1,23 +1,52 @@
-let firstCard = 10;
-let secondCard = 1;
-let cards = [firstCard, secondCard]
-
-let sum = firstCard + secondCard;
-let hasBlackJack = false;
+let cards = [];
+let sum = 0;
 let message = "";
-let isAlive = true;
+
+let hasBlackJack = false;
+let isAlive = false;
 
 let messageEl = document.getElementById("message-el");
-// let sumEl = document.getElementById("sum-el");
 let sumEl = document.querySelector("#sum-el");
 let cardsEl = document.querySelector("#cards-el");
 
+let player = {
+  name: "ARS",
+  chips: 145,
+};
+
+let playerEl = document.getElementById("player-el");
+playerEl.textContent = player.name + ": $" + player.chips;
+
+function getRandomCard() {
+  let randomNum = Math.floor(Math.random() * 13) + 1; // return random between 1 to 13
+  if (randomNum === 1) {
+    // is ACE => 11
+    return 11;
+  } else if (randomNum > 10) {
+    // if K(13) / Q(12) / J(11) => 10
+    return 10;
+  } else {
+    // 2 - 10
+    return randomNum;
+  }
+}
+
 function startGame() {
+  isAlive = true;
+  let firstCard = getRandomCard();
+  let secondCard = getRandomCard();
+  cards = [firstCard, secondCard];
+  sum = firstCard + secondCard;
+
   renderGame();
 }
 
 function renderGame() {
-  cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1];
+  cardsEl.textContent = "Cards: ";
+  for (let i = 0; i < cards.length; i++) {
+    cardsEl.textContent += cards[i] + " ";
+  }
+
   sumEl.textContent = "Sum: " + sum;
   if (sum <= 20) {
     message = "Almost! Draw a new card?";
@@ -33,8 +62,15 @@ function renderGame() {
 }
 
 function newCard() {
-  let newCard = 3;
-  sum += newCard;
+  if (!isAlive || hasBlackJack) {
+    return;
+  }
+
+  let card = getRandomCard();
+  sum += card;
+  cards.push(card);
+
   renderGame();
 }
-08:40:41
+
+// Till 09:51:05
